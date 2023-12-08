9adef2de867076cc95fb226f98155163e4b1d94e
yourname <rugor@users.noreply.github.com>
2023-12-08 11:46:25 -0800

    add favicons

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -9,6 +9,24 @@ export default defineConfig({
   base: '/grunt-digital-audit/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    // logo: '/logo.svg',
+    lastUpdated: {
+      text: 'Updated at',
+      formatOptions: {
+        dateStyle: 'full',
+        timeStyle: 'medium'
+      }
+    },
+    externalLinkIcon: true,
+    head: [
+      ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
+      ['link', { rel: 'icon', sizes: '32x32', type: 'image/png', href: '/favicon-32x32.png' }],
+      ['link', { rel: 'icon', sizes: '16x16', type: 'image/png', href: '/favicon-16x16.png' }],
+      ['link', { rel: 'manifest', href: '/site.webmanifest' }],
+      ['link', { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }],
+      ['meta', { name: 'msapplication-TileColor', content: '#ffffff' }],
+      ['meta', { name: 'theme-color', content: '#ffffff' }]
+    ],
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Examples', link: '/markdown-examples' }
